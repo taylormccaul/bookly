cbd1ea2b8fbc1c94487ba9724e64fa6e88795a7c
Taylor <paigeemccaul@gmail.com>
2020-06-29 16:33:36 -0400

    Add 'add new habit' function

--- src/App.js
+++ src/App.js
@@ -5,23 +5,52 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      userInput: "",
+      habits: []
     }
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClick() {
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
 
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let newHabit = {
+      text: this.state.userInput,
+      key: Date.now()
+    }
+
+    this.setState({
+      habits: [...this.state.habits, newHabit.text]
+    });
   }
 
   render() {
     return (
-      <div>
+      <div className="app">
         <header>
           <h1>Habit Tracker</h1>
-          <button type="submit" onClick={this.handleClick}>+</button>
         </header>
+        <form onSubmit={this.handleSubmit} className="new-habit">
+          <input type="text" className="add-new-habit" placeholder="Add a new habit" value={this.state.userInput} onChange={this.handleChange} />
+          <button type="submit">+</button>
+        </form>
+        <div className="habits">
+          <ul>
+            {this.state.habits.map((habit) => {
+              return (
+                <li key={habit}>{habit}</li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
